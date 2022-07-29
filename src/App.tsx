5339a777af2353ea5e6f634657c47ca722534beb
danglephd <danglephd@gmail.com>
2022-07-29 13:47:41 +0700

    Add react table

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -8,11 +7,28 @@ import {
 } from "react-router-dom";
 import Detail from './detail';
 import { useEffect, useState } from 'react';
+import { useTable } from 'react-table';
 
 function App() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  const columns = [{
+    id: 'Id',
+    header: 'ID',
+    accessor: 'Id'
+  }, {
+    header: 'User ID',
+    accessor: 'userId',
+  }, {
+    header: 'Title',
+    accessor: 'title'
+  }, {
+    header: 'Action',
+    accessor: 'title'
+  }];
+  
 
   useEffect(() => {
     const getData = async () => {
@@ -28,6 +44,7 @@ function App() {
         let actualData = await response.json();
         setData(actualData);
         setError(null);
+        console.log('>>>>>', actualData)
       } catch(err) {
         // setError(err.message);
         setData(null);
@@ -38,6 +55,47 @@ function App() {
     getData()
   }, [])
 
+  function Table({ columns, data } : any) {
+    // Use the state and functions returned from useTable to build your UI
+    const {
+      getTableProps,
+      getTableBodyProps,
+      headerGroups,
+      rows,
+      prepareRow,
+    } = useTable({
+      columns,
+      data,
+    })
+  
+    // Render the UI for your table
+    return (
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    )
+  }
+  
   return (
     
     <div>
@@ -50,6 +108,9 @@ function App() {
       >
         <Link to="/detail">Detail</Link> 
       </nav>
+
+      <Table columns={columns} data={data} />
+      
     </div>
   );
 }
